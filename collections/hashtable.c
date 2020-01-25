a9596f0776422c1711e1887cea04c7bddf858e3d
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2020-01-25 13:34:35 -0500

    Adds ADT Interface definition

--- collections/hashtable.c
+++ collections/hashtable.c
@@ -1,5 +1,20 @@
 //
 // Created by Alex Kneipp on 1/25/20.
 //
+#include <stdlib.h>
+#include <stdint.h>
+
+struct HashTable{
+    void** keys;
+    void** values;
+    size_t size;
+    size_t mem_size;
+    uint32_t (*hash)(void*);
+    int (*key_equals)(void*, void*);
+    int (*value_equals)(void*, void*);
+    float fill_ratio;
+};
 
 #include "hashtable.h"
+
+
--- collections/hashtable.h
+++ collections/hashtable.h
@@ -5,4 +5,26 @@
 #ifndef INC_2020_CORE_CODE_HASHTABLE_H
 #define INC_2020_CORE_CODE_HASHTABLE_H
 
+typedef struct HashTable * HashTable_t;
+
+HashTable_t hashtable_init();
+HashTable_t hashtable_initSize(size_t size);
+
+int hashtable_put(HashTable_t ht, void* key, void* value);
+int hashtable_putIfAbsent(HashTable_t ht, void* key, void* value);
+
+void* hashtable_get(HashTable_t ht, void* key);
+
+int hashtable_remove(HashTable_t ht, void* key);
+int hashtable_removeIfValue(HashTable_t ht, void* key, void* value);
+
+int hashtable_replace(HashTable_t ht, void* key, void* newValue);
+int hashtable_replaceIfValue(HashTable_t ht, void* key, void* oldValue, void* newValue);
+
+int hashtable_containsKey(HashTable_t ht, void* key);
+int hashtable_containsValue(HashTable_t ht, void* value);
+
+size_t hashtable_size(HashTable_t ht);
+int hashtable_isEmpty(HashTable_t ht);
+
 #endif //INC_2020_CORE_CODE_HASHTABLE_H
