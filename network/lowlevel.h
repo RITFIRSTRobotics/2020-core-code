f4d64d7fe21489298fe0bdf305a3f28c72f3f60c
Connor Henley <thatging3rkid@users.noreply.github.com>
2020-01-26 02:43:30 -0500

    put acceptor thread back

--- network/lowlevel.h
+++ network/lowlevel.h
@@ -52,6 +52,22 @@ typedef struct NetworkConnection {
 } NetworkConnection_t;
 
 /**
+ * Starts a thread that accepts new TCP connections. After a new connection is
+ * started, the thread sets up the data structure for this connection and starts
+ * the listeners for TCP and UDP.
+ *
+ * @param (*on_connect) the handler function that is called after every new
+ *        connection. The function is passed a pointer to a structure that can
+ *        contains connection data and can be used to send a packet.
+ * @param (*on_packet) the handler function that gets called on every incoming
+ *        packet. This function is passed a pointer to a TLV structure that
+ *        contains the recieved data.
+ * @note this should only be run on the server-side of the connection (i.e. FMS)
+ */
+void llnet_acceptor_start(void (*on_connect)(NetworkConnection_t*),
+    void (*on_packet)(IntermediateTLV_t*));
+
+/**
  * Sets up a listener to listen to TCP packets
  *
  * @param connection the connection that the listener should listen on. The
