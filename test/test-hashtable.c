f75c69203afcd55f93876bea46fe57718f0bc81e
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2020-01-26 19:52:18 -0500

    Adds a bunch of tests

--- test/test-hashtable.c
+++ test/test-hashtable.c
@@ -18,6 +18,7 @@
 
 #define TEST_SUCCESS 0
 #define TEST_FAILURE 1
+#define MAX_TEST_SIZE 1024
 
 static uint32_t hash(void* uint)
 {
@@ -76,14 +77,14 @@ int t03_initInitialSize()
 int t04_putGetNoInitialSize()
 {
     int result = TEST_SUCCESS;
-    void* testVals[1024];
+    void* testVals[MAX_TEST_SIZE];
     HashTable_t ht = hashtable_init(hash, key_equals, value_equals);
-    for(unsigned long i = 0; i < 1024; i++)
+    for(unsigned long i = 1; i < MAX_TEST_SIZE; i++)
     {
         testVals[i] =(void*) i;
         hashtable_put(ht, testVals[i], testVals[i]);
     }
-    for(unsigned long i = 0; i<1024; i++)
+    for(unsigned long i = 0; i<MAX_TEST_SIZE; i++)
     {
         if(hashtable_get(ht, (void*)i) != (void*)i)
         {
@@ -97,14 +98,14 @@ int t04_putGetNoInitialSize()
 int t05_putGetInitialSizeNoRehash()
 {
     int result = TEST_SUCCESS;
-    void* testVals[1024];
-    HashTable_t ht = hashtable_initSize(hash, key_equals, value_equals, 2048);
-    for(unsigned long i = 0; i < 1024; i++)
+    void* testVals[MAX_TEST_SIZE];
+    HashTable_t ht = hashtable_initSize(hash, key_equals, value_equals, MAX_TEST_SIZE * 2);
+    for(unsigned long i = 1; i < MAX_TEST_SIZE; i++)
     {
         testVals[i] =(void*) i;
         hashtable_put(ht, testVals[i], testVals[i]);
     }
-    for(unsigned long i = 0; i<1024; i++)
+    for(unsigned long i = 0; i<MAX_TEST_SIZE; i++)
     {
         void* ht_result = hashtable_get(ht, (void*)i);
         if(ht_result!= (void*)i)
@@ -117,26 +118,147 @@ int t05_putGetInitialSizeNoRehash()
     return result;
 }
 
+int t06_putGetWithDefault()
+{
+    int result = TEST_SUCCESS;
+    void* testVals[MAX_TEST_SIZE];
+    HashTable_t ht = hashtable_initSize(hash, key_equals, value_equals, MAX_TEST_SIZE * 2);
+    for(unsigned long i = 0; i < MAX_TEST_SIZE; i++)
+    {
+        testVals[i] =(void*) i;
+        hashtable_put(ht, testVals[i], testVals[i]);
+    }
+    //NULL (=0) is not allowed as a key
+    for(unsigned long i = 1; i<MAX_TEST_SIZE; i++)
+    {
+        void* ht_result = hashtable_getWithDefault(ht, (void*)i, (void*)i+1);
+        if(ht_result!= (void*)i)
+        {
+            result += TEST_FAILURE;
+            printf("%d:%d\n",(int)ht_result, (int)(i));
+        }
+    }
+    void* defaultGet = hashtable_getWithDefault(ht, (void*) MAX_TEST_SIZE + 1, NULL);
+    if(defaultGet != NULL)
+    {
+        result += TEST_FAILURE;
+    }
+    //Test with another default value so we're sure it isn't always returning null
+    defaultGet = hashtable_getWithDefault(ht, (void*) MAX_TEST_SIZE + 1, (void*)1);
+    if(defaultGet != (void*)1)
+    {
+        result += TEST_FAILURE;
+    }
+    hashtable_destroy(ht);
+    return result;
+}
+
+int t07_hashtableReturnsFailureOnNullKeyInsert()
+{
+    int result = TEST_SUCCESS;
+    HashTable_t ht = hashtable_initSize(hash, key_equals, value_equals, MAX_TEST_SIZE * 2);
+    int putResult;
+    if(putResult = hashtable_put(ht, NULL, NULL) != 0)
+    {
+        printf("putResult: %d\n", putResult);
+        result += TEST_FAILURE;
+    }
+    hashtable_destroy(ht);
+    return result;
+}
+
 int main() {
     // Run tests on both types of list
     int error = 0;
+    int allErrors = 0;
     printf("Starting Test01!\n");
-    error += t01_initNoValueEquals();
+    error = t01_initNoValueEquals();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
     printf("Starting Test02!\n");
-    error += t02_initValueEquals();
+    error = t02_initValueEquals();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
     printf("Starting Test03!\n");
-    error += t03_initInitialSize();
+    error = t03_initInitialSize();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
     printf("Starting Test04!\n");
-    error += t04_putGetNoInitialSize();
+    error = t04_putGetNoInitialSize();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
     printf("Starting Test05!\n");
-    error += t05_putGetInitialSizeNoRehash();
+    error = t05_putGetInitialSizeNoRehash();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
+    printf("Starting Test06!\n");
+    error = t06_putGetWithDefault();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
+
+    printf("Starting Test07!\n");
+    error = t07_hashtableReturnsFailureOnNullKeyInsert();
+    if(error == 0 )
+    {
+        printf("success!\n");
+    }
+    else
+    {
+        printf("^^^ test errors\n");
+    }
+    allErrors += error;
 
     // Tests finished, handle the error code
-    if (error == 0) {
-        printf("success!\n");
+    if (allErrors == 0) {
         return EXIT_SUCCESS;
     } else {
-        printf("^^^ test errors\n");
         return EXIT_FAILURE;
     }
 }
