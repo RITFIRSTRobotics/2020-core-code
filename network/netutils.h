8b712770ff5e92d2d1752edc27f3cd39d1c7109f
Connor Henley <thatging3rkid@users.noreply.github.com>
2020-09-19 19:51:01 -0400

    fix weird spacing in netutils.h

--- network/netutils.h
+++ network/netutils.h
@@ -15,51 +15,53 @@
 #ifdef __cplusplus
 extern "C" {
 #endif //__cplusplus
-    /**
-     * Create a new KVPairTLV.
-     * @param key
-     *  The key in the KV pair. This can be safely free'd after this function returns
-     * @param type
-     *  The type of Value the KV pair holds
-     * @param length
-     *  The length of the TLV structure in bytes.
-     * @param value
-     *  The value to store in the kvPair. If \p type is kv_CString, this may be free'd after this function returns.
-     * @return
-     *  A pointer to a new KVPairTLV_t, with \p key as its key, \p type as its type,
-     *  \p length as its length, and \p value as its value.
-     */
-    KVPairTLV_t* KVPairTLV_create(const char* key, KVPair_Type_t type, size_t length, KVPair_Value_u value);
 
-    /**
-     * Create a new KVPairTLV from a memory address containing a KV-TLV as defined in the protocol definition
-     * @param start
-     *  A pointer to the start of a KVPairTLV in a buffer.
-     * @param end
-     *  A pointer to the byte after the last byte of the KVPairTLV pointed to by start.
-     *  This will be set to NULL if the most recent call did not contain a KVPairTLV structure.
-     * @return
-     *  A pointer to a new KVPairTLV_t with values extracted from memory.
-     */
-    KVPairTLV_t* KVPairTLV_createFromMemory(const char* start, char** end);
+/**
+ * Create a new KVPairTLV.
+ * @param key
+ *  The key in the KV pair. This can be safely free'd after this function returns
+ * @param type
+ *  The type of Value the KV pair holds
+ * @param length
+ *  The length of the TLV structure in bytes.
+ * @param value
+ *  The value to store in the kvPair. If \p type is kv_CString, this may be free'd after this function returns.
+ * @return
+ *  A pointer to a new KVPairTLV_t, with \p key as its key, \p type as its type,
+ *  \p length as its length, and \p value as its value.
+ */
+KVPairTLV_t* KVPairTLV_create(const char* key, KVPair_Type_t type, size_t length, KVPair_Value_u value);
 
-    /**
-     * Check if two KVPairTLV's are equal.
-     * @param kv1
-     *  The first KVPairTLV to compare.
-     * @param kv2
-     *  The second KVPairTLV to compare.
-     * @return
-     *  1 if kv1 == kv2, 0 otherwise
-     */
-    int KVPairTLV_equals(KVPairTLV_t* kv1, KVPairTLV_t* kv2);
+/**
+ * Create a new KVPairTLV from a memory address containing a KV-TLV as defined in the protocol definition
+ * @param start
+ *  A pointer to the start of a KVPairTLV in a buffer.
+ * @param end
+ *  A pointer to the byte after the last byte of the KVPairTLV pointed to by start.
+ *  This will be set to NULL if the most recent call did not contain a KVPairTLV structure.
+ * @return
+ *  A pointer to a new KVPairTLV_t with values extracted from memory.
+ */
+KVPairTLV_t* KVPairTLV_createFromMemory(const char* start, char** end);
 
-    /**
-     * Destroy a KVPairTLV*.  If the type is kv_CString, the value field will be free'd.
-     * @param pairToDestroy
-     *  The KV pair to free the memory of.
-     */
-    void KVPairTLV_destroy(KVPairTLV_t* pairToDestroy);
+/**
+ * Check if two KVPairTLV's are equal.
+ * @param kv1
+ *  The first KVPairTLV to compare.
+ * @param kv2
+ *  The second KVPairTLV to compare.
+ * @return
+ *  1 if kv1 == kv2, 0 otherwise
+ */
+int KVPairTLV_equals(KVPairTLV_t* kv1, KVPairTLV_t* kv2);
+
+/**
+ * Destroy a KVPairTLV*.  If the type is kv_CString, the value field will be free'd.
+ * @param pairToDestroy
+ *  The KV pair to free the memory of.
+ */
+void KVPairTLV_destroy(KVPairTLV_t* pairToDestroy);
+    
 #ifdef __cplusplus
 }
 #endif //__cplusplus
